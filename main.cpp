ef234d74b6f48ac5deae61333877170710903ce2
rokpie <91060774+rokpie@users.noreply.github.com>
2021-09-23 09:37:31 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <math.h>
+#include "graphics.h"
+
+using namespace std;
+
+int sveikasskaicius = 8; ///Cia sveikas skaicius
+
+const int WIDTH = 800, HEIGHT = 800;
+void drawLine ( int moveToX, int moveToY, int drawX, int drawY, color, textX, textY, char *name){
+
+}
+
+void funkcija(){
+    /// Èia yra funkcija
+}
+
+double polinomas(double x){
+    double rezultatas = 2*x*x +10*x+15;
+    return rezultatas;
+}
+
+int main()
+{
+/*
+    cout << "Hello world!" << endl;
+    //funkcijapointmain();
+
+    for (double x = -10; x <= 10; x+=0.1){
+            cout << x << " -> " << polinomas(x)<< endl;
+    }
+    */
+return 0;
+}
+
+/*
+void funkcijapointmain (){ /// cia yra funkcija po int main
+    cout << publicsveikasskaicius;
+}
+*/
